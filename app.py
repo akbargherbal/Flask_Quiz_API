f2024c9efaf1f9895d44e055e4ae4434b2180e7c
akbargherbal <51210927+akbargherbal@users.noreply.github.com>
2022-11-30 23:32:50 +0300

    First Upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask,jsonify,request
+import pandas as pd
+file_path = 'VERB_QUIZ_007.csv'
+df = pd.read_csv(file_path, encoding='utf-8')
+
+quiz_set_flask = df.sample(frac=1).reset_index(drop=True)
+quiz_set_flask = quiz_set_flask.to_dict('records')
+
+
+app = Flask(__name__)
+
+@app.route('/returnjson', methods = ['GET'])
+def ReturnJSON():
+	if(request.method == 'GET'):
+
+		return jsonify(quiz_set_flask)
+
+if __name__=='__main__':
+	app.run(debug=True)
